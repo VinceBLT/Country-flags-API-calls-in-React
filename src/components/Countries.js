f90511ab562625c9518917a73d45d44ca7fbb548
Vincent <vincent.bezault@icloud.com>
2021-03-23 18:13:01 +0100

    final props and style

---
+++ src/components/Countries.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Card } from "./Card";
+
+export const Countries = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
+      )
+      .then((res) => setData(res.data));
+    console.log(data);
+  }, []);
+
+  return (
+    <div className="countries">
+      <ul className="countries-list">
+        {data.map((country) => (
+          <Card country={country} key={country.name}/>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Logo = () => {
+    return (
+        <div className="logo">
+            <img src="./img/logo.png" alt="logo"/>
+            <h3>React World</h3>
+        </div>
+    )
+}

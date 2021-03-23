9d46b707dca07da2e43c43285c363065e4516477
Vincent <vincent.bezault@icloud.com>
2021-03-23 18:40:49 +0100

    radio buttons

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -7,6 +7,8 @@ export const Countries = () => {
   const [sortedData, setSortedData] = useState([]);
   const [playOnce, setPlayOnce] = useState(true);
   const [rangeValue, setRangeValue] = useState(40);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   useEffect(() => {
     if (playOnce) {
@@ -41,11 +43,29 @@ export const Countries = () => {
           value={rangeValue}
           onChange={(e) => setRangeValue(e.target.value)}
         />
+        <ul>
+          {radios.map((radio) => {
+            return (
+              <li key={radio}>
+                <input
+                  type="radio"
+                  value={radio}
+                  id={radio}
+                  checked={radio === selectedRadio}
+                  onChange={(e) => setSelectedRadio(e.target.value)}
+                />
+                <label htmlFor={radio}>{radio}</label>
+              </li>
+            );
+          })}
+        </ul>
       </div>
       <ul className="countries-list">
-        {sortedData.map((country) => (
-          <Card country={country} key={country.name} />
-        ))}
+        {sortedData
+          .filter((country) => country.region.includes(selectedRadio))
+          .map((country) => (
+            <Card country={country} key={country.name} />
+          ))}
       </ul>
     </div>
   );

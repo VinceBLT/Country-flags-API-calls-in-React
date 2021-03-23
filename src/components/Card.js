0bcabfe116e44fc448124634a257343b61180b2f
Vincent <vincent.bezault@icloud.com>
2021-03-23 18:15:39 +0100

    add number k separator for Population

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,21 @@ import React from "react";
 
 export const Card = (props) => {
   const { country } = props;
-  console.log(country);
+
+  const numberFormat = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
 
   return (
     <li className="card">
-        <img src={country.flag} alt="flag"/>
-        <div className="data-container">
-            <ul>
-                <li>{country.name}</li>
-                <li>{country.capital}</li>
-                <li>Pop. {country.population}</li>
-            </ul>
-        </div>
+      <img src={country.flag} alt="flag" />
+      <div className="data-container">
+        <ul>
+          <li>{country.name}</li>
+          <li>{country.capital}</li>
+          <li>Pop. {numberFormat(country.population)}</li>
+        </ul>
+      </div>
     </li>
-    );
+  );
 };
